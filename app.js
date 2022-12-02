56870ed4d3da8effe27e55123f3a78cce7978ff8
Farid Ahmed <73395538+Farid4hmed@users.noreply.github.com>
2022-12-02 17:54:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true}, {useUnifiedTopology: true});
 
 const postSchema = {
 
